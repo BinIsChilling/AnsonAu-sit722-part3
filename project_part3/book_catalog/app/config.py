8de63035fea93df54b3bf5c22c7089eaa53dcc25
BinIsChilling <auanson2918@gmail.com>
2024-09-16 18:36:22 +1000

    g

--- project_part3/book_catalog/app/config.py
+++ project_part3/book_catalog/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://books_inventory_user:9hNFJrnxrxlTVYcGkiVFuBEhvu5hFNoh@dpg-criiimm8ii6s73f45to0-a.oregon-postgres.render.com/books_inventory')
